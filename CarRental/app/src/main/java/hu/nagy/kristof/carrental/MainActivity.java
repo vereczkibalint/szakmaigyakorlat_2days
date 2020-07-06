95c62f6f358737cd24d062a0e5b7476f95b69e73
Gulyás Péter <gulyaspeter20@gmail.com>
2020-07-06 15:44:49 +0200

    LoginLayout

--- CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
+++ CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button singUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Intent intent = new Intent(MainActivity.this, Autok.class);
-        startActivity(intent);
+        singUpButton=(Button)findViewById(R.id.buttonSignUp);
+        singUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Autok.class);
+                startActivity(intent);      
+            }
+        });
     }
 }
