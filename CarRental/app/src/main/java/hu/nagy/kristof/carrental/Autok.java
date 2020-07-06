a8f4efb98d2ac4e3a25cfbe4a791277ecd95552a
nagykris1997 <55376303+nagykris1997@users.noreply.github.com>
2020-07-06 14:30:55 +0200

    as
    d

---
+++ CarRental/app/src/main/java/hu/nagy/kristof/carrental/Autok.java
@@ -0,0 +1,14 @@
+package hu.nagy.kristof.carrental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Autok extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autok);
+    }
+}
