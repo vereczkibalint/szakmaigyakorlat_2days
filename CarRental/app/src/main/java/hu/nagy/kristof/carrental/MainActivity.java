28554e898a56b18a00f21b5a8653c1a0427d0235
nagykris1997 <55376303+nagykris1997@users.noreply.github.com>
2020-07-07 10:14:08 +0200

    redesign update

--- CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
+++ CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,15 +32,11 @@ public class MainActivity extends AppCompatActivity {
         singUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(checkForEmptyData()){
-                    Toast.makeText(MainActivity.this, "Minden mező kitöltése kötelező!", Toast.LENGTH_LONG).show();
-                }else if(checkForInvalidEmail()){
-                    Toast.makeText(MainActivity.this, "Hibás email formátum!", Toast.LENGTH_LONG).show();
-                }else if(checkForInvalidPhoneNumber()){
-                    Toast.makeText(MainActivity.this, "Hibás telefonszám formátum!", Toast.LENGTH_LONG).show();
-                }else{
+                if(validateData()){
                     setUserDataPrefs();
                     loadCarsActivity();
+                }else{
+                    Toast.makeText(MainActivity.this, "Minden mező kitöltése kötelező!", Toast.LENGTH_LONG).show();
                 }
             }
         });
@@ -57,16 +51,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    protected boolean checkForEmptyData(){
-        return TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(phoneNumber.getText().toString()) || TextUtils.isEmpty(name.getText().toString());
-    }
-
-    protected boolean checkForInvalidEmail(){
-        return !Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches();
-    }
-
-    protected boolean checkForInvalidPhoneNumber(){
-        return !phoneNumber.getText().toString().matches("\\+36[0-9]{9}");
+    protected boolean validateData(){
+        return !(TextUtils.isEmpty(email.getText().toString()) || TextUtils.isEmpty(phoneNumber.getText().toString()) || TextUtils.isEmpty(name.getText().toString()));
     }
 
     protected void setUserDataPrefs(){
