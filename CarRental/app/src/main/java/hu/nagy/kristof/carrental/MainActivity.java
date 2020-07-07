20c84d239b2c4c25a8acc2b5a6ebd5d8b2e684d1
vereczkibalint <vereczkibalint@gmail.com>
2020-07-07 10:25:48 +0200

    changed commit() to apply()

--- CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
+++ CarRental/app/src/main/java/hu/nagy/kristof/carrental/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -60,6 +61,6 @@ public class MainActivity extends AppCompatActivity {
         userPrefEditor.putString("email", email.getText().toString());
         userPrefEditor.putString("phoneNumber", phoneNumber.getText().toString());
         userPrefEditor.putString("name", name.getText().toString());
-        userPrefEditor.commit();
+        userPrefEditor.apply();
     }
 }
